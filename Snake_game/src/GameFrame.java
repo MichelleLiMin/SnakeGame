9908b7eeacfc53f1302f536059c01da9ef26ceb7
michelleitaly <michelle22289@gmail.com>
2022-10-18 22:38:03 +0200

    Youtube course 1

---
+++ Snake_game/src/GameFrame.java
@@ -0,0 +1,15 @@
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+	
+	GameFrame(){
+		GamePanel panel = new GamePanel();
+		this.add(new GamePanel());
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+}
